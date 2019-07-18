2c5e3d8e944ae619bd967c35a3fbb6685a396004
abdu020 <you@example.com>
2019-07-18 17:33:34 +0200

    Refactored the app from a class based component to a functional component with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import axios from "axios";
@@ -7,97 +7,91 @@ import Data from "./components/weather-info/Data";
 import Spinner from "./components/layout/Spinner";
 import Navbar from "./components/layout/Navbar";
 
-class App extends Component {
-  state = {
-    weather: [],
-    loading: false,
-    isButtonDisabled: false,
-    message: ""
-  };
+const App = () => {
+  // State with hooks
+  const [weather, setWeather] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [message, setMessage] = useState("");
 
-  showData = async () => {
-    this.setState({ loading: true });
+  const showData = async () => {
+    setLoading(true);
     const res = await axios.get(
       "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c5dd4d4949520c4f85675def7c5a3a41/55.676098,12.568337"
     );
 
     // Removing the spinner after 2 seconds
     setTimeout(() => {
-      this.setState({ loading: false });
+      setLoading(false);
     }, 2000);
 
     // Fetching the data after 2 seconds
     setTimeout(() => {
-      this.setState({ weather: res.data.currently });
-      this.setState({
-        message: [
-          "Summary: ",
-          "Temperature: ",
-          "Dew Point: ",
-          "Humidity: ",
-          "Ozone: ",
-          "Current Pressure: ",
-          "Visibility: ",
-          "Wind Bearing",
-          "Wind Gust: ",
-          "Wind Speed: "
-        ]
-      });
+      setWeather(res.data.currently);
+
+      setMessage([
+        "Summary: ",
+        "Temperature: ",
+        "Dew Point: ",
+        "Humidity: ",
+        "Ozone: ",
+        "Current Pressure: ",
+        "Visibility: ",
+        "Wind Bearing",
+        "Wind Gust: ",
+        "Wind Speed: "
+      ]);
     }, 2000);
 
     // disabling the button after 2 seconds
     setTimeout(() => {
-      this.setState({ isButtonDisabled: true });
+      setIsButtonDisabled(true);
     }, 2000);
   };
 
-  render() {
-    // Destructuring
-    const { weather, loading, message, isButtonDisabled } = this.state;
-    return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route
-              path="/"
-              render={props => (
-                <Fragment>
-                  <Ui
-                    spinner={loading && <Spinner />}
-                    ButtonDisabled={isButtonDisabled}
-                    summaryText={message}
-                    temperatureText={message}
-                    dewPointText={message}
-                    humidityText={message}
-                    ozoneText={message}
-                    currentPressureText={message}
-                    visibilityText={message}
-                    windBearingText={message}
-                    windGustText={message}
-                    windSpeedText={message}
-                    showData={this.showData}
-                    summary={weather.summary}
-                    temperature={weather.apparentTemperature}
-                    dewPoint={weather.dewPoint}
-                    humidity={weather.humidity}
-                    icon={weather.icon}
-                    ozone={weather.ozone}
-                    pressure={weather.pressure}
-                    visibility={weather.visibility}
-                    windBearing={weather.windBearing}
-                    windGust={weather.windGust}
-                    windSpeed={weather.windSpeed}
-                  />
-                  <Data />
-                </Fragment>
-              )}
-            />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route
+            path="/"
+            render={props => (
+              <Fragment>
+                <Ui
+                  spinner={loading && <Spinner />}
+                  ButtonDisabled={isButtonDisabled}
+                  summaryText={message}
+                  temperatureText={message}
+                  dewPointText={message}
+                  humidityText={message}
+                  ozoneText={message}
+                  currentPressureText={message}
+                  visibilityText={message}
+                  windBearingText={message}
+                  windGustText={message}
+                  windSpeedText={message}
+                  showData={showData}
+                  summary={weather.summary}
+                  temperature={weather.apparentTemperature}
+                  dewPoint={weather.dewPoint}
+                  humidity={weather.humidity}
+                  icon={weather.icon}
+                  ozone={weather.ozone}
+                  pressure={weather.pressure}
+                  visibility={weather.visibility}
+                  windBearing={weather.windBearing}
+                  windGust={weather.windGust}
+                  windSpeed={weather.windSpeed}
+                />
+                <Data />
+              </Fragment>
+            )}
+          />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
